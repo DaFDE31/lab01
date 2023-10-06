82357260a7fb40d1b066fc5825cdd9c55d826acd
DaFDE31 <adjocydanielson@gmail.com>
2023-10-06 00:11:14 +0000

    hiii

--- lab1.c
+++ lab1.c
@@ -1,5 +1,14 @@
-/*
- * Implement your solution in thi file
- *
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lab1.h"
 
+char* readString(char* fileName){
+    return fileName;
+}
+
+char* mysteryExplode(const char* str){
+    int len = strlen(str);
+    int strlen = (len * (len ));
+    return NULL;
+}
