bd50be9195946cc30118850b1105077686afbd5f
DaFDE31 <adjocydanielson@gmail.com>
2023-10-09 03:47:40 +0000

    EXPLOSION

--- lab1.c
+++ lab1.c
@@ -4,13 +4,21 @@
 #include "lab1.h"
 
 char* readString(char* fileName){
-    return fileName;
+    char* ret = (char*) malloc(100);
+
+    FILE *fileptr = fopen(fileName, "r");
+
+    fgets(ret, MAX_LINE_LEN, fileptr);
+
+    fclose(fileptr);
+    ret[strcspn(ret, "\n")] = '\0';
+    return ret;
 }
 
 char* mysteryExplode(const char* str){
     int len = strlen(str);
-    char *exploded = (char*) malloc((len*(len+1)/2)+1);
-    memset(exploded, '\0', (len*(len+1)/2+1));
+    char* exploded = (char*) malloc((len * (len+1)/2)+1);
+    memset(exploded, '\0', (len * (len+1)/2+1));
     for (int x = 1; x <=len; x++){
         //strncat adds from second argument onto first by third arguments amount of characters
         strncat(exploded, str, x);
